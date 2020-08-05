0d5ee5362944b8638c446cf04dd3dfb5ffe983d1
Miguel Acosta <miguel.acosta1978@gmail.com>
2020-08-04 22:50:28 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e5188a47e2c0a3856907dc1f544b288'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3503717-6a87-4159-90d8-3e7aef4d35b2'
+};
